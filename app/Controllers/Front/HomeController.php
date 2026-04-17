873b01c4859092642f2e6feab44c1b97776a28cc
ArchSeraphin <trinity.informatique@gmail.com>
2026-04-17 11:30:48 +0200

    feat(seo): integrate Seo + SchemaBuilder into base layout via partials

--- app/Controllers/Front/HomeController.php
+++ app/Controllers/Front/HomeController.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 namespace App\Controllers\Front;
 
-use App\Core\{Container, Request, Response, View};
+use App\Core\{Container, Request, Response, View, Config};
+use App\Services\{Seo, SchemaBuilder, Settings};
 
 final class HomeController
 {
@@ -10,13 +11,53 @@ final class HomeController
     {
         /** @var View $view */
         $view = Container::get(View::class);
-        return new Response($view->render('front/home.html.twig'));
+        $url  = rtrim((string)Config::get('APP_URL', ''), '/') . '/';
+        $siteName = Settings::get('site_name', 'Site');
+        $seo = Seo::build([
+            'site_name'   => $siteName,
+            'title'       => null,
+            'description' => Settings::get('seo_default_description') ?: Settings::get('site_description'),
+            'url'         => $url,
+            'image'       => Settings::get('seo_og_image'),
+        ]);
+        $schemas = [
+            SchemaBuilder::localBusiness([
+                'name'    => $siteName,
+                'type'    => Settings::get('localbusiness_type', 'LocalBusiness'),
+                'url'     => $url,
+                'phone'   => Settings::get('contact_phone'),
+                'email'   => Settings::get('contact_email'),
+                'address' => [
+                    'street'  => Settings::get('contact_address'),
+                    'city'    => Settings::get('contact_city'),
+                    'postal'  => Settings::get('contact_postal_code'),
+                    'country' => Settings::get('contact_country', 'FR'),
+                ],
+                'geo' => [
+                    'lat' => Settings::get('localbusiness_geo_lat'),
+                    'lng' => Settings::get('localbusiness_geo_lng'),
+                ],
+            ]),
+            SchemaBuilder::website([
+                'name' => $siteName,
+                'url'  => $url,
+            ]),
+        ];
+        return new Response($view->render('front/home.html.twig', [
+            'seo'     => $seo,
+            'schemas' => $schemas,
+        ]));
     }
 
     public function notFound(Request $req): Response
     {
         /** @var View $view */
         $view = Container::get(View::class);
-        return new Response($view->render('front/404.html.twig'), 404);
+        $seo = Seo::build([
+            'site_name' => Settings::get('site_name', 'Site'),
+            'title'     => 'Page introuvable',
+            'url'       => rtrim((string)Config::get('APP_URL', ''), '/') . $req->path,
+        ]);
+        return new Response($view->render('front/404.html.twig', ['seo' => $seo]), 404);
     }
 }
